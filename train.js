478d5eb0de965d2e8a3013eaf3e06f81020a8da7
Cheng <git@zcbenz.com>
2024-07-16 16:54:49 +0900

    Shuffle the training data

--- train.js
+++ train.js
@@ -4,7 +4,7 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import {existsSync} from 'node:fs'
 import prettyMilliseconds from 'pretty-ms'
-import {ParquetReader} from '@dsnp/parquetjs'
+import {ParquetGroupReader} from '@frost-beta/parquet-reader'
 import {TokenizerLoader} from '@lenml/tokenizers'
 import {core as mx, optimizers as optim, nn, utils} from '@frost-beta/mlx'
 
@@ -19,7 +19,6 @@ if (process.argv.length < 3) {
 const contextSize = 256
 
 // Traning configs.
-const epochs = 1
 const batchSize = 32
 const learningRate = 1e-4
 const maxRows = Infinity
@@ -53,9 +52,12 @@ async function main() {
   console.log(`Training Llama3 with ${(nparams / 1024 ** 2).toFixed(1)}M parameters.`)
 
   // Command line flags.
-  const files = shuffle(process.argv.slice(2))
-  const totalRows = Math.min(maxRows, await getRowCount(files))
+  const files = process.argv.slice(2)
   const reportPerIter = Math.max(Math.floor(32 / batchSize * 10), 1)
+
+  // Prepare dataset.
+  const reader = new ParquetGroupReader(files)
+  const totalRows = Math.min(maxRows, await reader.getRowsCount())
   console.log('Total rows of data to train:', totalRows)
 
   // Preprare utils for doing gradient descent.
@@ -63,86 +65,68 @@ async function main() {
   const optimizer = new optim.AdamW(learningRate)
 
   // Read batches from the datasets.
+  let iter = 0
+  let start = Date.now()
   let lastRow = 0
   let losses = []
-  for (let e = 0, iterations = 0, start = Date.now(); e < epochs; ++e) {
-    for await (const [row, x, y] of iterateBatches(files, tokenizer, contextSize, batchSize)) {
-      if (lastRow > maxRows)
-        break
-      // Use mx.tidy to free all the intermediate tensors immediately.
-      mx.tidy(() => {
-        // Compute loss and gradients, then update the model.
-        const [loss, grads] = lossAndGradFunction(model, mx.array(x, mx.int32), mx.array(y, mx.int32))
-        optimizer.update(model, grads)
-        mx.eval(model.state, optimizer.state)
-        losses.push(loss.item())
-        // Keep the states of model and optimizer from getting freed.
-        return [model.state, optimizer.state]
-      })
-      // Report updates.
-      if (++iterations % reportPerIter === 0) {
-        const stop = Date.now()
-        const trainLoss = mean(losses)
-        const processedRows = e * totalRows + row
-        const eta = ((epochs * totalRows - processedRows) / (processedRows - lastRow)) * (stop - start)
-        console.log(`Iter ${iterations}`,
-                    `(${(100 * processedRows / (epochs * totalRows)).toFixed(1)}%):`,
-                    `Train loss ${trainLoss.toFixed(2)},`,
-                    `It/sec ${(reportPerIter / (stop - start) * 1000).toFixed(2)},`,
-                    `ETA ${prettyMilliseconds(eta, {compact: true})}.`)
-        start = Date.now()
-        losses = []
-        lastRow = processedRows
-      }
+  for await (const [row, x, y] of iterateBatches(reader, tokenizer, contextSize, batchSize)) {
+    if (lastRow > maxRows)
+      break
+    // Use mx.tidy to free all the intermediate tensors immediately.
+    mx.tidy(() => {
+      // Compute loss and gradients, then update the model.
+      const [loss, grads] = lossAndGradFunction(model, mx.array(x, mx.int32), mx.array(y, mx.int32))
+      optimizer.update(model, grads)
+      mx.eval(model.state, optimizer.state)
+      losses.push(loss.item())
+      // Keep the states of model and optimizer from getting freed.
+      return [model.state, optimizer.state]
+    })
+    // Report updates.
+    if (++iter % reportPerIter === 0) {
+      const stop = Date.now()
+      const trainLoss = mean(losses)
+      const eta = (totalRows - row) / (row - lastRow) * (stop - start)
+      console.log(`Iter ${iter}`,
+                  `(${(100 * row / totalRows).toFixed(1)}%):`,
+                  `Train loss ${trainLoss.toFixed(2)},`,
+                  `It/sec ${(reportPerIter / (stop - start) * 1000).toFixed(2)},`,
+                  `ETA ${prettyMilliseconds(eta, {compact: true})}.`)
+      start = Date.now()
+      losses = []
+      lastRow = row
     }
   }
+  await reader.close()
 
   // Save weights on exit.
   console.log(`Save weights to ${weightsFile}.`)
   model.saveWeights(weightsFile)
 }
 
-// Return the total number of rows.
-async function getRowCount(files) {
-  let count = 0
-  for (const f of files) {
-    const reader = await ParquetReader.openFile(f)
-    count += parseInt(reader.getRowCount())
-    await reader.close()
-  }
-  return count
-}
-
 // Read datasets from |files|, and generate batches of [inputs, targets].
-async function* iterateBatches(files, tokenizer, contextSize, batchSize) {
+async function* iterateBatches(reader, tokenizer, contextSize, batchSize) {
   let row = 0
   let xBatches = []
   let yBatches = []
-  for (const f of files) {
-    // Read the dataset.
-    const reader = await ParquetReader.openFile(f)
-    const cursor = reader.getCursor()
-    let record
-    while (record = await cursor.next()) {
-      ++row
-      // Convert text to tokens.
-      const tokens = tokenizer.encode(record.text)
-      // Generate batches from the tokens.
-      for (let i = 0; i < tokens.length - 1; i += contextSize) {
-        const length = Math.min(contextSize, tokens.length - i - 1)
-        // If the batch's length is less than contextSize, fill it with EOS.
-        let paddings = []
-        if (length < contextSize)
-          paddings = new Array(contextSize - length).fill(tokenizer.model.eosTokenId)
-        xBatches.push(tokens.slice(i, i + length).concat(paddings))
-        yBatches.push(tokens.slice(i + 1, i + 1 + length).concat(paddings))
-      }
-      // Yield batches with each batch of |batchSize|.
-      while (xBatches.length >= batchSize) {
-        yield [ row, xBatches.splice(0, batchSize), yBatches.splice(0, batchSize) ]
-      }
+  for await (const data of await reader.getIterator({shuffle: true})) {
+    ++row
+    // Convert text to tokens.
+    const tokens = tokenizer.encode(data[3])
+    // Generate batches from the tokens.
+    for (let i = 0; i < tokens.length - 1; i += contextSize) {
+      const length = Math.min(contextSize, tokens.length - i - 1)
+      // If the batch's length is less than contextSize, fill it with EOS.
+      let paddings = []
+      if (length < contextSize)
+        paddings = new Array(contextSize - length).fill(tokenizer.model.eosTokenId)
+      xBatches.push(tokens.slice(i, i + length).concat(paddings))
+      yBatches.push(tokens.slice(i + 1, i + 1 + length).concat(paddings))
     }
-    await reader.close()
+    // Yield batches with each batch of |batchSize|.
+    while (xBatches.length >= batchSize) {
+      yield [row, xBatches.splice(0, batchSize), yBatches.splice(0, batchSize)]
+    }
   }
 }
 
@@ -159,14 +143,4 @@ function mean(array) {
   if (array.length == 0)
     return 0
   return array.reduce((a, b) => a + b) / array.length
-}
-
-// Shuffle the elements in a JS array.
-function shuffle(array) {
-  const result = array.slice()
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]]
-  }
-  return result
 }
